5a41f2cd22e4cd0976346f8a8706b36f8ff45702
quandaworld <46980777+quandaworld@users.noreply.github.com>
2023-08-23 15:40:13 -0700

    Adjust for loop begin and condition

--- javascript.js
+++ javascript.js
@@ -41,7 +41,7 @@ function game() {
   let playerWin = 0;
   let computerWin = 0;
 
-  for (let i = 1; i <= 5; i++) {
+  for (let i = 0; i < 5; i++) {
     const playerInput = prompt("Rock, Paper, Scissors?").toLowerCase();
     const computerSelection = getComputerChoice();
     const result = playRound(playerInput, computerSelection);
